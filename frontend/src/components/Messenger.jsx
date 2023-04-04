8a68d19b2fe83be0e7d1c84f075c7361777f7543
AviDasta <avidasta1993@gmail.com>
2023-04-04 16:05:00 +0300

    change the state for user that choosen

--- frontend/src/components/Messenger.jsx
+++ frontend/src/components/Messenger.jsx
@@ -74,7 +74,11 @@ const Messenger = () => {
                 ? friends.map((fd) => (
                     <div
                       onClick={() => setCurrentFriend(fd)}
-                      className="hover-friend"
+                      className={
+                        currentFriend._id === fd._id
+                          ? "hover-friend active"
+                          : "hover-friend"
+                      }
                     >
                       <Friends friend={fd} />
                     </div>
